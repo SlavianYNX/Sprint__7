a8a4c72a821355bc2d8a35ae5fa2bb7100699dac
Slava Eremin <slavik.eremin@yandex.ru>
2025-06-05 14:32:30 +0300

    Обновление

--- tests/test_creating_courier.py
+++ tests/test_creating_courier.py
@@ -13,13 +13,11 @@ class TestCourierMethods:
         r = create_courier
         assert r.status_code == 201 and r.json() == TestMessages.COURIER_SUCCESSFUL_CREATION
 
-
     @allure.title('Проверка получения сообщения об ошибке при создания двух одинаковых курьеров')
     @allure.description('Отправка запроса на создание двух курьеров с одинаковыми логинами, получение сообщения об ошибке')
     def test_two_creating_courier(self, courier, create_courier, courier_delete):
         w = ApiMethods.create_courier(courier)
         assert w.status_code == 409 and w.json() == TestMessages.COURIER_LOGIN_ALREADY_IN_USE
-
 
     @allure.title('Проверка создания курьера при пустом поле "login"')
     @allure.description('Отправка запроса на создание курьера без заполнения поля login, получение сообщения об ошибке ')
