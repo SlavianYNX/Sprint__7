d46e90fe4064197f40480f7ed0db0ebb5a76a9b6
Slava Eremin <slavik.eremin@yandex.ru>
2025-06-05 14:31:24 +0300

    Вынос в фискстуры повторяющихся шагов.Добавление тестов на два поля

--- tests/test_creating_courier.py
+++ tests/test_creating_courier.py
@@ -1,6 +1,6 @@
 import pytest
 import allure
-from conftest import courier
+from conftest import courier, courier_delete, create_courier
 from methods_api import ApiMethods
 from data import TestMessages
 
@@ -9,24 +9,35 @@ class TestCourierMethods:
 
     @allure.title('Проверка создания нового курьера')
     @allure.description('Отправка запроса на создание курьера. Получение сообщения об успешном создании курьера')
-    def test_creating_courier(self, courier):
-        r = ApiMethods.create_courier(courier)
+    def test_creating_courier(self, courier, create_courier, courier_delete):
+        r = create_courier
         assert r.status_code == 201 and r.json() == TestMessages.COURIER_SUCCESSFUL_CREATION
-        p = ApiMethods.login_courier(courier['login'], courier['password'])
-        ApiMethods.delete_courier(p.json()['id'])
+
 
     @allure.title('Проверка получения сообщения об ошибке при создания двух одинаковых курьеров')
     @allure.description('Отправка запроса на создание двух курьеров с одинаковыми логинами, получение сообщения об ошибке')
-    def test_two_creating_courier(self, courier):
-        ApiMethods.create_courier(courier)
-        r = ApiMethods.create_courier(courier)
-        assert r.status_code == 409 and r.json() == TestMessages.COURIER_LOGIN_ALREADY_IN_USE
+    def test_two_creating_courier(self, courier, create_courier, courier_delete):
+        w = ApiMethods.create_courier(courier)
+        assert w.status_code == 409 and w.json() == TestMessages.COURIER_LOGIN_ALREADY_IN_USE
 
-    @allure.title('Проверка создания курьера при пустом поле login')
+
+    @allure.title('Проверка создания курьера при пустом поле "login"')
     @allure.description('Отправка запроса на создание курьера без заполнения поля login, получение сообщения об ошибке ')
-    def test_creating_courier_no_login(self, courier):
+    def test_creating_courier_no_login(self, courier, create_courier):
         courier["login"] = ""
         r = ApiMethods.create_courier(courier)
         assert r.status_code == 400 and r.json() == TestMessages.COURIER_NOT_ENOUGH_REGISTER_DATA
 
+    @allure.title('Проверка создания курьера при пустом поле "password"')
+    @allure.description('Отправка запроса на создание курьера без заполнения поля password, получение сообщения об ошибке')
+    def test_creating_courier_no_password(self, courier):
+        courier["password"] = ""
+        r = ApiMethods.create_courier(courier)
+        assert r.status_code == 400 and r.json() == TestMessages.COURIER_NOT_ENOUGH_REGISTER_DATA
 
+    @allure.title('Проверка создания курьера при пустом поле "firstName"')
+    @allure.description('Отправка запроса на создание курьера без заполнения поля "firstName"')
+    def test_creating_courier_no_firstname(self, courier):
+        courier["firstName"] = ""
+        r = ApiMethods.create_courier(courier)
+        assert r.status_code == 201 and r.json() == TestMessages.COURIER_SUCCESSFUL_CREATION
